ed82fb7afb327e65485f415234831562043da7cb
liuzhuanwei <liuzhuanwei@qq.com>
2020-10-06 14:25:17 +0800

    聊天室项目初始化

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './pages/login/Login';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Login/>, rootElement);
